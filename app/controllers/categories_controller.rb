fc3b5dce59c3b59d4a3b4733d939e2dd70d25668
julien-evrard-rennes <julien.dominique.evrard@gmail.com>
2025-12-03 15:02:52 +0100

    Catégories créées

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,42 +1,41 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Categorie.all
+    @categories = Category.all
   end
 
   def show
-    @categorie = Categorie.find(params[:id])
+    @category = Category.find(params[:id])
   end
 
   def edit
-    @categorie = Categorie.find(params[:id])
+    @category = Category.find(params[:id])
   end
 
   def update
-    @categorie = Categorie.find(params[:id])
-    categorie_params = params.require(:categorie).permit(:label, :content)
-    @categorie.update(categorie_params)
-    redirect_to categorie_path(@categorie.id)
+    @category = Category.find(params[:id])
+    @category.update(categorie_params)
+    redirect_to categories_path
   end
 
   def new
-    @categorie = Categorie.new
+    @category = Category.new
   end
 
   def create
-    categorie = Categorie.create(categorie_params)
-    redirect_to categories_path(categorie.id)
+    @category = Category.create(categorie_params)
+    redirect_to categories_path
   end
 
   def destroy
-    @categorie = Categorie.find(params[:id])
+    @category = Category.find(params[:id])
 
-    @categorie.destroy
+    @category.destroy
     redirect_to categories_path
   end
 
   private
 
   def categorie_params
-    params.require(:categorie).permit(:label, :content)
+    params.require(:category).permit(:label, :content)
   end
 end
